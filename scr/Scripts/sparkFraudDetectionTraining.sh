ee0e4e90ab50ce989ae399642138b089c27e2a38
SAINATH DUTKAR <31874079+SainathDutkar@users.noreply.github.com>
2019-07-22 02:43:21 -0400

    Update sparkFraudDetectionTraining.sh

--- scr/Scripts/sparkFraudDetectionTraining.sh
+++ scr/Scripts/sparkFraudDetectionTraining.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #Training Spark Job
-spark-submit --class com.datamantra.spark.jobs.FraudDetectionTraining --name "Fraud Detection Spark ML Training" --master spark://datamantra:7077 $HOME/frauddetection/spark/fruaddetection-spark.jar  $HOME/frauddetection/spark/application-local.conf
+spark-submit --class com.fraudDetection.spark.jobs.FraudDetectionTraining --name "Fraud Detection Spark ML Training" --master spark://fraudDetection:7077 $HOME/frauddetection/spark/fruaddetection-spark.jar  $HOME/frauddetection/spark/application-local.conf
