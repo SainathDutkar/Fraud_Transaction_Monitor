5c9446b0297210e70d0644755427c5a7f7c7cbe1
SAINATH DUTKAR <31874079+SainathDutkar@users.noreply.github.com>
2019-07-22 02:40:47 -0400

    Update sparkRealtimeFraudDectionStructuredSteaming.sh

--- scr/Scripts/sparkRealtimeFraudDectionStructuredSteaming.sh
+++ scr/Scripts/sparkRealtimeFraudDectionStructuredSteaming.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #Structured Streaming Spark Job
-spark-submit --class com.datamantra.spark.jobs.RealTimeFraudDetection.StructuredStreamingFraudDetection --name "RealTime Creditcard FraudDetection Structured Streaming" --master spark://datamantra:7077 --deploy-mode cluster  $HOME/frauddetection/spark/fruaddetection-spark.jar $HOME/frauddetection/spark/application-local.conf
+spark-submit --class com.fraudDetection.spark.jobs.RealTimeFraudDetection.StructuredStreamingFraudDetection --name "RealTime Creditcard FraudDetection Structured Streaming" --master spark://fraudDetection:7077 --deploy-mode cluster  $HOME/frauddetection/spark/fruaddetection-spark.jar $HOME/frauddetection/spark/application-local.conf
